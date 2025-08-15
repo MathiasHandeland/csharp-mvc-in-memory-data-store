f631317e2d674c2171c000269195ba57fe988aee
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-08-15 14:43:26 +0200

    added check for integer in request in both update and add endpoints

--- exercise.wwwapi/Endpoints/ProductEndpoints.cs
+++ exercise.wwwapi/Endpoints/ProductEndpoints.cs
@@ -3,6 +3,7 @@ using exercise.wwwapi.Models;
 using exercise.wwwapi.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace exercise.wwwapi.Endpoints
 {
@@ -14,9 +15,9 @@ namespace exercise.wwwapi.Endpoints
 
             app.MapGet("/{id}", GetProductById); // endpoint for requesting a product
             app.MapGet("/", GetProducts); // endpoint for requesting all products in the database, optionally all products from a category
-            app.MapPost("/", AddProduct); // endpoint for adding a product to the database
+            app.MapPost("/", AddProduct).Accepts<ProductPost>("application/json"); ; // endpoint for adding a product to the database
             app.MapDelete("/{id}", DeleteProduct); // endpoint for deleting a spesific product
-            app.MapPut("/{id}", UpdateProduct); // endpoint for updating a spesific product
+            app.MapPut("/{id}", UpdateProduct).Accepts<ProductPut>("application/json"); ; // endpoint for updating a spesific product
         }
 
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -55,17 +56,24 @@ namespace exercise.wwwapi.Endpoints
 
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public static async Task<IResult> AddProduct(IProductRepository repository, ProductPost model)
+        public static async Task<IResult> AddProduct(IProductRepository repository, HttpRequest request)
         {
+            // check if the value the client provided for price is an integer
+            ProductPost? model;
+            try
+            {
+                model = await request.ReadFromJsonAsync<ProductPost>(); // read the request body the client provided and deserialize it to ProductPost model
+            }
+            catch (JsonException) // if the JSON is not valid, e.g. the price is not an int we catch it and return a bad request
+            {
+                return Results.BadRequest("Price must be a number, something else was provided");
+            }
             // check if the string the client provided for name doesn`t already exists in the database
             var existingsProduct = await repository.GetAsync(null); // no category provided, so we check all products if the name already exists
             if (existingsProduct.Any(p => p.Name.Equals(model.Name, StringComparison.OrdinalIgnoreCase)))
             {
                 return TypedResults.BadRequest($"Product with name {model.Name} already exists");
             }
-
-            // TODO check if the value the client provided for price is an integer
-           
 
             Product entity = new Product(); // create a new product entity to add to the database
             entity.Category = model.Category; // many products can have the same category so no need to check if the category is already in the database
@@ -81,11 +89,29 @@ namespace exercise.wwwapi.Endpoints
 
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public static async Task<IResult> UpdateProduct(IProductRepository repository, ProductPut model, int id)
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public static async Task<IResult> UpdateProduct(IProductRepository repository, HttpRequest request, int id)
         {
+            ProductPut? model;
+            try
+            {
+                model = await request.ReadFromJsonAsync<ProductPut>();
+            }
+            catch (JsonException)
+            {
+                return Results.BadRequest("Price must be a number, something else was provided");
+            }
+
             Product existingProduct = await repository.GetByIdAsync(id);
             if (existingProduct == null) return TypedResults.NotFound($"Product with ID {id} not found.");
-            
+
+            // check if the string the client provided for name doesn`t already exists in the database
+            var existingsProduct = await repository.GetAsync(null); // no category provided, so we check all products if the name already exists
+            if (existingsProduct.Any(p => p.Name.Equals(model.Name, StringComparison.OrdinalIgnoreCase)))
+            {
+                return TypedResults.BadRequest($"Product with name {model.Name} already exists");
+            }
+
             // update the existing product with the new values from the model the client sent
             if (model.Name != null) { existingProduct.Name = model.Name; }
             if (model.Category != null) { existingProduct.Category = model.Category; }
