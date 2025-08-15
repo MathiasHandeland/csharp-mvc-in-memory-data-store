de7b9a0698e73547b4df69be485d29aaff1ddb09
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-08-15 10:32:03 +0200

    added properties to the product class

--- exercise.wwwapi/Models/Product.cs
+++ exercise.wwwapi/Models/Product.cs
@@ -1,6 +1,14 @@
-﻿namespace exercise.wwwapi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace exercise.wwwapi.Models
 {
     public class Product
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+
     }
 }
